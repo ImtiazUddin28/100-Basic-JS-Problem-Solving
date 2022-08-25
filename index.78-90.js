964f6cbefe8924c361e2f10d30f8c8afae9a48be
ImtiazUddin28 <imtiaz100463@gmail.com>
2022-08-25 22:50:04 +0600

    79 to 84 questions are added

---
+++ index.78-90.js
@@ -0,0 +1,31 @@
+// 78) Write an arrow function that will take 3 parameters, will multiply
+// the parameters and will return the result.
+
+// 79) Write the following sentence in three lines and print the result:
+// I am a web developer. I love to code. I love to eat biryani.
+
+// 80) Write an arrow function that will take 2 parameters: One parameter
+// will come from you and the other parameter will be a default
+// parameter. Add these two parameters and return the result.
+
+
+
+// 81) It will take an array where the array elements will be the
+// name of your friends
+
+// 82) Check if the length of each element is even, push elements
+// with even length to a new array and return the result
+// Print the result.
+
+// 83) Write an arrow function where it will do the following:
+// a) Square each array element
+// b) Calculate the sum of the squared elements
+// c) Return the average of the sum of the squared elements
+// Print the result.
+
+
+// 84) Write an arrow function where it will do the following:
+// a) It will take two array inputs
+// b) Combine the two arrays and assign them in a new array
+// c) Find the maximum number from the new array and return the
+// result
